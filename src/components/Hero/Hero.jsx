a18c6500d74716f881c54bca5ad1ae616f15faa6
juniorpinhodev <juniorpinhodev@gmail.com>
2025-03-13 20:01:09 -0300

    feat: 🎸 Ajusta responsividade em Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,9 +18,9 @@ const Hero = () => {
         <div className='bg-gradient-to-r from-primary to bg-primaryDark/90'>
             <div className='container text-white'>
                 {/* Layout para desktop (visível apenas em telas médias e grandes) */}
-                <section className='hidden md:grid grid-cols-1 md:grid-cols-2 place-items-center min-h-[800px]'>
-                    {/* Texto */}
-                    <div className='space-y-8'>
+                <section className='hidden md:grid grid-cols-1 md:grid-cols-2 min-h-[800px]'>
+                    {/* Texto - Ajustado para alinhar à esquerda */}
+                    <div className='space-y-8 self-center justify-self-start'>
                         <motion.div
                             initial={{ opacity: 0, y: -100 }}
                             whileInView={{ opacity: 1, y: 0 }}
@@ -73,44 +73,49 @@ const Hero = () => {
                         </motion.div>
                     </div>
 
-                    {/* Imagem */}
-                    <div className='relative'>
-                        <UpdateFollower
-                            mouseOptions={{
-                                backgroundColor: 'white',
-                                zIndex: 1000,
-                                followSpeed: 1.6,
-                                scale: 5,
-                                mixBlendMode: 'soft-light',
-                            }}
-                        >
-                            <motion.img 
-                                initial={{ opacity: 0, x: 200 }}
-                                whileInView={{ opacity: 1, x: 7, scale: 1 }}
-                                transition={{ duration: 1 }}
-                                src={VictorPng} 
-                                alt="Victor Souza" 
-                                className="w-full max-w-[360px] relative z-30 h-auto"
-                                style={{
-                                    WebkitMaskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 83%)',
-                                    maskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 90%)'
-                                }} 
-                            />
-                        </UpdateFollower>
-
-                        {/* Círculo com sombras */}
-                        <motion.div 
-                            initial={{ opacity: 0, y: 0, scale: 0.5 }}
-                            whileInView={{ opacity: 1, y: 0, scale: 1 }}
-                            transition={{ 
-                                type: 'spring',
-                                stiffness: 100,
-                                duration: 0.2,
-                                damping: 10,
-                                delay: 0.8,
-                            }}
-                            className='border-[20px] border-cyan-200 rounded-full h-[180px] w-[180px] md:h-[300px] md:w-[300px] absolute top-3 left-1/4 glowing-shadow glowing-shadow-inset' 
-                        />
+                    {/* Imagem e círculo em um container para manter o posicionamento relativo */}
+                    <div className='relative self-center'>
+                        {/* Container para manter a imagem e o círculo juntos - Modificado para fixar posições relativas */}
+                        <div className="relative flex justify-center items-center">
+                            <UpdateFollower
+                                mouseOptions={{
+                                    backgroundColor: 'white',
+                                    zIndex: 1000,
+                                    followSpeed: 1.6,
+                                    scale: 5,
+                                    mixBlendMode: 'soft-light',
+                                }}
+                            >
+                                <div className="relative">
+                                    <motion.img 
+                                        initial={{ opacity: 0, x: 200 }}
+                                        whileInView={{ opacity: 1, x: 0, scale: 1 }}
+                                        transition={{ duration: 1 }}
+                                        src={VictorPng} 
+                                        alt="Victor Souza" 
+                                        className="w-full max-w-[360px] relative z-30 h-auto mx-auto"
+                                        style={{
+                                            WebkitMaskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 83%)',
+                                            maskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 90%)'
+                                        }} 
+                                    />
+                                    
+                                    {/* Círculo com sombras - REPOSICIONADO mais à direita e mais abaixo - Visível apenas em desktop */}
+                                    <motion.div 
+                                        initial={{ opacity: 0, scale: 0.5 }}
+                                        whileInView={{ opacity: 1, scale: 1 }}
+                                        transition={{ 
+                                            type: 'spring',
+                                            stiffness: 100,
+                                            duration: 0.2,
+                                            damping: 10,
+                                            delay: 0.8,
+                                        }}
+                                        className='border-[20px] border-cyan-200 rounded-full absolute glowing-shadow glowing-shadow-inset z-10 w-[300px] h-[300px] top-[10px] left-[90px]' 
+                                    />
+                                </div>
+                            </UpdateFollower>
+                        </div>
 
                         {/* Texto de fundo */}
                         <motion.div 
@@ -130,7 +135,7 @@ const Hero = () => {
 
                         <motion.div 
                             initial={{ opacity: 0, y: 100 }}
-                            whileInView={{ opacity: 1, x: 227, y: 106 }}
+                            whileInView={{ opacity: 1, y: -110 }}
                             transition={{ 
                                 type: 'spring',
                                 stiffness: 100,
@@ -138,17 +143,19 @@ const Hero = () => {
                                 damping: 10,
                                 delay: 1.6,
                             }}
-                            className='absolute -top-40 left-0 z-[1]'
+                            className='absolute top-20 left-20 z-[1]'
                         >
                             <h1 className='text-[180px] font-bold text-cyan-200/10 leading-none'>Souza</h1>
                         </motion.div>
                     </div>
                 </section>
 
-                {/* Layout para mobile (visível apenas em telas pequenas) */}
+
+                {/* -- Layout para mobile (visível apenas em telas pequenas) -- */}
+
                 <section className='md:hidden grid grid-cols-1 min-h-[800px]'>
                     {/* Texto no topo esquerdo */}
-                    <div className='pt-12'>
+                    <div className='pt-12 pl-2'>
                         <motion.div
                             initial={{ opacity: 0, y: -100 }}
                             whileInView={{ opacity: 1, y: 0 }}
@@ -161,7 +168,7 @@ const Hero = () => {
                             }}
                             className='space-y-8'
                         >
-                            <h1 className='text-5xl font-bold leading-tight uppercase'>
+                            <h1 className='text-4xl font-bold leading-tight uppercase'>
                                 Victor <br /> {" "}<span className='text-transparent text-outline'>Souza</span>
                             </h1>
 
@@ -174,7 +181,7 @@ const Hero = () => {
                                     mixBlendMode: 'soft-light',
                                 }}
                             >
-                                <button className='outline-btn flex justify-center items-center gap-2 group'>
+                                <button className='outline-btn flex justify-center items-center gap-2 group w-[100px] px-6 py-2 text-[18px] font-semibold'>
                                     Explore
                                     <FaArrowRight className='group-hover:translate-x-2 transition'/>  
                                 </button>
@@ -182,39 +189,68 @@ const Hero = () => {
                         </motion.div>
                     </div>
                     
-                    {/* Imagem no topo direito */}
-                    <div className='relative mt-6 flex justify-end'>
-                        <motion.img 
-                            initial={{ opacity: 0, x: 200 }}
-                            whileInView={{ opacity: 1, x: 0, scale: 1 }}
-                            transition={{ duration: 1 }}
-                            src={VictorPng} 
-                            alt="Victor Souza" 
-                            className="max-w-[200px] w-auto h-auto relative z-30 object-contain"
-                            style={{
-                                WebkitMaskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 83%)',
-                                maskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 90%)'
-                            }} 
-                        />
-                        
+                    {/* Imagem no topo - Centralizada horizontalmente */}
+                    <div className='relative mt-6 flex justify-center'>
+                        <div className="relative">
+                            <motion.img 
+                                initial={{ opacity: 0, y: -230 }}
+                                whileInView={{ opacity: 1, y: -150, x: 70, scale: 1 }}
+                                transition={{ 
+                                    type: 'spring',
+                                    duration: 1.2,
+                                    stiffness: 10,
+                                    damping: 12
+                                }}
+                                src={VictorPng} 
+                                alt="Victor Souza" 
+                                className="max-w-[180px] xs:max-w-[200px] sm:max-w-[220px] w-auto h-auto relative z-30 object-contain"
+                                style={{
+                                    WebkitMaskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 83%)',
+                                    maskImage: 'linear-gradient(to top, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 90%)'
+                                }} 
+                            />
+                            
+                            {/* Círculo removido na versão mobile */}
+                        </div>
+                    </div>
+
+                    {/* Texto de fundo - Centralizado horizontalmente */}
+                    <div className="flex flex-col items-center">
                         <motion.div 
-                            initial={{ opacity: 0, y: 0, scale: 0.5 }}
-                            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                            initial={{ opacity: 0, y: 0 }}
+                            whileInView={{ opacity: 1, x: 100, y: -400 }} 
                             transition={{ 
                                 type: 'spring',
-                                stiffness: 100,
-                                duration: 0.2,
-                                damping: 10,
-                                delay: 0.8,
+                                stiffness: 50,
+                                duration: 0.8,
+                                damping: 30,
+                                delay: 1.4,
                             }}
-                            className='border-[15px] border-cyan-200 rounded-full h-[160px] w-[160px] absolute top-0 right-[20px] glowing-shadow glowing-shadow-inset' 
-                        />
+                            className='relative z-[1]'
+                        >
+                            <h1 className='text-[50px] xs:text-[55px] sm:text-[60px] font-bold text-cyan-200/10 leading-none'>Victor</h1>
+                        </motion.div>
+
+                        <motion.div 
+                            initial={{ opacity: 0, y: 0 }} 
+                            whileInView={{ opacity: 1, x: 140, y: -400 }} 
+                            transition={{ 
+                                type: 'spring',
+                                stiffness: 50,
+                                duration: 0.8,
+                                damping: 30,
+                                delay: 1.8,
+                            }}
+                            className='relative z-[1]'
+                        >
+                            <h1 className='text-[60px] xs:text-[65px] sm:text-[70px] font-bold text-cyan-200/10 leading-none'>Souza</h1>
+                        </motion.div>
                     </div>
-                    
+
                     {/* Investidor imobiliário abaixo do texto */}
                     <motion.div 
                         initial={{ opacity: 0, y: 100 }}
-                        whileInView={{ opacity: 1, y: 0 }}
+                        whileInView={{ opacity: 1, y: -90, x: 0 }}
                         transition={{ 
                             type: 'spring',
                             delay: 1,
@@ -222,7 +258,7 @@ const Hero = () => {
                             stiffness: 100,
                             damping: 7,
                         }}
-                        className='bg-black/40 p-6 space-y-2'
+                        className='bg-black/40 p-6 space-y-2 ml-2'
                     >
                         <h1 className='text-xl uppercase'>Investidor Imobiliário</h1>
                         <p className='text-xs leading-loose'>
